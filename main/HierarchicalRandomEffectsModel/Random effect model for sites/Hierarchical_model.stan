0f282d14e8e8d565b64ed7c5e6fe3752bbfc838c
MatteoPierdomenico <55437984+MatteoPierdomenico@users.noreply.github.com>
2021-01-20 18:07:43 +0100

    Start of the Hierarchical Random Effects model
    File stan for the hierarchical random effects model for sites

---
+++ main/HierarchicalRandomEffectsModel/Random effect model for sites/Hierarchical_model.stan
@@ -0,0 +1,155 @@
+// source: https://mc-stan.org/users/documentation/case-studies/gpareto_functions.html
+
+functions {
+  real FOMC_pdf(real x1,real x2,real threshold,real sigma,real xi,real alpha,real lambda){
+  if(x1>threshold && x2>threshold){
+    return -((1 + xi*(-threshold + x1)/sigma)^(1/xi)/lambda)^(-1/alpha)*((1 + xi*(-threshold + x2)/sigma)^(1/xi)/lambda)^(-1/alpha)*
+    (((1 + xi*(-threshold + x2)/sigma)^(1/xi)/lambda)^(-1/alpha) + ((1 + xi*(-threshold + x1)/sigma)^(1/xi)/lambda)^(-1/alpha))^alpha/
+    (sigma^2*(1 + xi*(-threshold + x1)/sigma)*(1 + xi*(-threshold + x2)/sigma)*(((1 + xi*(-threshold + x2)/sigma)^(1/xi)/lambda)^(-1/alpha) 
+    + ((1 + xi*(-threshold + x1)/sigma)^(1/xi)/lambda)^(-1/alpha))^2) + ((1 + xi*(-threshold + x1)/sigma)^(1/xi)/lambda)^(-1/alpha)*
+    ((1 + xi*(-threshold + x2)/sigma)^(1/xi)/lambda)^(-1/alpha)*(((1 + xi*(-threshold + x2)/sigma)^(1/xi)/lambda)^(-1/alpha) + 
+    ((1 + xi*(-threshold + x1)/sigma)^(1/xi)/lambda)^(-1/alpha))^alpha/(alpha*sigma^2*(1 + xi*(-threshold + x1)/sigma)*(1 +
+    xi*(-threshold + x2)/sigma)*(((1 + xi*(-threshold + x2)/sigma)^(1/xi)/lambda)^(-1/alpha) + ((1 + xi*(-threshold + x1)/sigma)^(1/xi)/lambda)^(-1/alpha))^2);
+  }
+  
+  
+  if(x1>threshold && x2<=threshold){
+    return (1/(lambda*sigma))*((xi*(x1-threshold)/sigma +1)^(1/xi-1))*(((1/lambda)*(xi*(x1-threshold)/sigma+1)^(1/xi))^(-1/alpha -1))*
+              ((((1/lambda)*(xi*(x1-threshold)/sigma+1)^(1/xi))^(-1/alpha) + ((1/lambda)*(xi*(threshold-threshold)/sigma+1)^(1/xi))^(-1/alpha))^(alpha-1));
+    
+  }
+  if(x1<=threshold && x2>threshold){
+    return (1/(lambda*sigma))*((xi*(x2-threshold)/sigma +1)^(1/xi-1))*(((1/lambda)*(xi*(x2-threshold)/sigma+1)^(1/xi))^(-1/alpha -1))*
+      ((((1/lambda)*(xi*(x2-threshold)/sigma+1)^(1/xi))^(-1/alpha) + ((1/lambda)*(xi*(threshold-threshold)/sigma+1)^(1/xi))^(-1/alpha))^(alpha-1));
+    
+  }
+  else{    
+    return 1-((((1/lambda)*(xi*(threshold-threshold)/sigma+1)^(1/xi))^(-1/alpha) + ((1/lambda)*(xi*(threshold-threshold)/sigma+1)^(1/xi))^(-1/alpha))^(alpha));
+    
+    
+  }
+  
+}
+
+real gpd_pdf(real x,real threshold,real scale,real xi, real lambda){
+  if(x>=threshold)
+  {if (fabs(xi) > 1e-15)
+    return lambda*(scale^(-1)) * (1 + (xi * (x - threshold))/scale)^(-(+1/xi + 1));
+  else
+    return  lambda*(1/scale)*exp(-(x-threshold)/scale);}
+  else{
+    return (1-lambda)/threshold;
+  }
+}
+
+real gpd_lpdf(real x,real threshold,real scale,real xi, real lambda){
+  return log(gpd_pdf(x,threshold, scale,xi, lambda)); 
+}
+
+
+real gpareto_lpdf(vector y, real ymin, real xi, real sigma) {
+    // generalised Pareto log pdf 
+    int N = rows(y);
+    real inv_xi = inv(xi);
+    if (xi<0 && max(y-ymin)/sigma > -inv_xi)
+      reject("xi<0 and max(y-ymin)/sigma > -1/xi; found xi, sigma =", xi, sigma);
+    if (sigma<=0)
+      reject("sigma<=0; found sigma =", sigma);
+    if (fabs(xi) > 1e-15)
+      return -(1+inv_xi)*sum(log1p((y-ymin) * (xi/sigma))) -N*log(sigma);
+    else
+      return -sum(y-ymin)/sigma -N*log(sigma); // limit xi->0
+  }
+
+
+real Hmodel_lpdf(vector x,real threshold,real scale,real xi,real lambda,real alpha){
+  int N = rows(x);
+  real likelihood = log(gpd_pdf(x[1], threshold,scale,xi, lambda));
+  real num= 0;
+  real denom =0;
+  for (i in 1:(N-1)) {
+    num = log(FOMC_pdf(x[i],x[i+1],  threshold,scale,xi, alpha, lambda)) +num;
+    denom = log(gpd_pdf(x[i],  threshold,scale,xi,lambda)) + denom;
+  }
+  likelihood = likelihood+ num -denom;
+  return likelihood;
+} 
+
+}
+data {
+  int<lower=0> N;
+  int<lower=0> M;
+  matrix[N,M] y;
+  vector<lower=0>[M] threshold;
+  vector<lower=0>[M] lambda;
+  real b;
+  real c;
+  real f;
+  real g;
+  // int<lower=0> Nt;
+  // vector<lower=ymin>[Nt] yt;
+}
+
+transformed data{
+  real<lower=0> tau_a;
+  tau_a =1/c;
+  
+}
+
+
+parameters {
+  real a_sigma;
+  real a_xi;
+  real<lower=0.005,upper=0.015> phi_sigma;
+  real<lower=0.05,upper=0.15> phi_xi;
+  vector<lower=0,upper=1>[M] alpha;
+  vector<lower=0>[M] sigma; 
+  vector<lower=0>[M] xi_star; 
+
+}
+transformed parameters{
+  vector[M] xi;
+  real<lower=0> tau_sigma;
+  real<lower=0> tau_xi;
+  tau_sigma =(1/phi_sigma);
+  tau_xi =(1/phi_xi);
+  for(i in 1:M){
+    xi[i] = xi_star[i] - sigma[i]/(max(y[,i])-threshold[i]);
+    
+  }
+}
+
+  
+
+model {
+  //layer 3
+  a_sigma ~ normal(b,tau_a);
+  a_xi ~ normal(b,tau_a);
+  //phi_sigma ~ gamma(f,g);    
+  //phi_xi ~ gamma(f,g);
+  phi_sigma ~ uniform(0.005,0.015);
+  phi_xi ~ uniform(0.05,0.15);
+  
+  //layer 2 
+  
+  for(i in 1:M){
+    target += normal_lpdf(sigma[i] | a_sigma,tau_sigma);
+    target += normal_lpdf(xi[i] | a_xi,tau_xi);
+  }
+  //sigma ~ lognormal(a_sigma,1/phi_sigma);
+  //xi ~ normal(a_xi,1/phi_xi);
+  alpha ~ uniform(0,1);
+  //layer 1
+  
+  for(i in 1:M){
+    target += Hmodel_lpdf(y[,i]| threshold[i], sigma[i], xi[i],lambda[i], alpha[i]);
+  }
+  //for(i in 1:M){
+  //  target += gpareto_lpdf(y[,i]| threshold[i], xi[i], sigma[i]);
+  //}
+  //
+  //for(i in 1:M){
+  //  y[,i] ~ gpareto(threshold[i],xi[i],sigma[i]);
+  //}
+  //
+}
---
+++ main/HierarchicalRandomEffectsModel/Random effect model for sites/Hierarchical_model_stan.R
@@ -0,0 +1,226 @@
+library(rstan)
+library(coda)
+library(tidyverse)
+library(POT)
+library(evir)
+library(loo)
+
+
+Ireland <- read.csv('C:/Users/matte/OneDrive/Documenti/GitHub/Bayesian-Approach-to-EVT/Dataset/Ireland_daily_from1990.csv')
+head(Ireland)
+unique(Ireland$spot)
+
+df <- data.frame(time=Ireland$year, obs=Ireland$hg, county=Ireland$spot)
+n <- length(df$obs)
+m <- length(unique(df$county))
+
+spots <- list()
+data <- list()
+counties <- as.vector(unique(Ireland$spot))
+for(i in 1:m){ 
+county <- Ireland[which(Ireland$spot==counties[i]),]
+spots[[i]] <- data.frame(time=county$year, obs=county$hg)
+data[[i]] <- county$hg
+plot(pacf(spots[[i]]$obs))
+readline(prompt = "press [enter] to continue or ESC to stop")
+graphics.off()
+}
+
+
+
+thresholds <- rep(NA,m)
+lambda <- rep(NA,m)
+
+
+
+i=5#loop manuale (modificare i fino a 5)
+
+# initial data selection
+initial_threshold <- 91
+events0 <- c(spots[[i]][which(spots[[i]]$obs>initial_threshold),])
+events0 <- data.frame(time = events0$time,obs= events0$obs, idx=which(spots[[i]]$obs>initial_threshold))
+# threshold selection (try 12)
+par(mfrow = c(2, 2))
+mrlplot(events0[, "obs"]) # should be linear after threshold
+abline(v = initial_threshold, col = "green")
+diplot(events0) # should be close to 1
+abline(v = initial_threshold, col = "green")
+tcplot(events0[, "obs"], which = 1) # should be constant after threshold
+abline(v = initial_threshold, col = "green")
+tcplot(events0[, "obs"], which = 2) # should be constant after threshold
+abline(v = initial_threshold, col = "green")
+graphics.off()
+
+thresholds[i] <- initial_threshold
+lambda[i] <- length(spots[[i]][which(spots[[i]]$obs>initial_threshold),2])/length(spots[[i]]$obs)
+
+tim.cond <- 3/365
+
+# cluster to avoid short range temporal dependence
+for(i in 1:m){
+spots[[i]] <- clust(spots[[i]], u = thresholds[i], tim.cond = tim.cond, 
+                 clust.max = TRUE, plot = TRUE)
+}
+
+
+#MCMC
+
+y <- cbind(spots[[1]][,2],spots[[2]][,2],spots[[3]][,2],spots[[4]][,2],spots[[5]][,2])
+N <- length(y[,1])
+M <- length(spots)
+b <- 0
+c <- 10^-3
+g<-f<-10^-2
+data_win <- list( N = N, M=M, y = y, threshold = thresholds, lambda=lambda,b=b,c=c,f=f,g=g)
+fit <- stan(file = "Hierarchical_model.stan", data = data_win, warmup = 1000, iter = 5000, 
+            chains = 2, thin = 10,seed = 199) 
+is(fit)
+print(fit, par = c('a_sigma','phi_sigma','a_xi','phi_xi','sigma', 'xi','alpha')) 
+
+
+
+
+#PLOT dell 3 catene generate per ogni parametro
+rstan::traceplot(fit, pars='a_sigma', inc_warmup = TRUE)
+rstan::traceplot(fit, pars='phi_sigma', inc_warmup = TRUE)
+rstan::traceplot(fit, pars='a_xi', inc_warmup = TRUE)
+rstan::traceplot(fit, pars='phi_xi', inc_warmup = TRUE)
+rstan::traceplot(fit, pars='sigma', inc_warmup = TRUE)
+rstan::traceplot(fit, pars='xi', inc_warmup = TRUE)
+rstan::traceplot(fit, pars='alpha', inc_warmup = TRUE)
+
+par <- rstan::extract(fit, pars = c('a_sigma','phi_sigma', 'a_xi','phi_xi',"sigma", "xi","alpha", "lp__"), inc_warmup = FALSE)
+is(par)
+names(par)
+
+sigmacb <- par$sigma
+xicb <- par$xi
+alphacb <- par$alpha
+
+lp <- par$lp__
+
+plot(sigmacb)
+plot(xicb)
+plot(alphacb)
+plot(lp)
+
+#PLOT della stima dei parametri
+
+plot_post <- fit %>% 
+  rstan::extract(c("sigma", "xi")) %>% 
+  as_tibble() %>% 
+  map_df(as_data_frame, .id = 'param')
+
+plot_post %>% 
+  ggplot(aes(value, fill = param)) + 
+  geom_density() + 
+  facet_wrap(~param, scales = 'free') + 
+  scale_fill_locuszoom() + 
+  theme_minimal() + 
+  theme(legend.position="bottom")
+
+pairs(fit, pars = c( 'sigma', 'xi', 'alpha'), condition = 0.5)
+
+#CODA analysis of the chains
+
+coda_chain <- As.mcmc.list(fit, pars = c('a_sigma','phi_sigma','a_xi','phi_xi',"sigma", "xi", 'alpha'))
+summary(coda_chain)
+gelman.diag(coda_chain, confidence = 0.95,  autoburnin = TRUE, multivariate=TRUE)
+x11()
+plot(coda_chain)
+acfplot(coda_chain)
+cumuplot(coda_chain)
+graphics.off()
+
+geweke.diag(coda_chain)
+geweke.plot(coda_chain, frac1 = 0.1, frac2 = 0.5, nbins = 20)
+
+
+###
+#CREATING A LIST OF THE MAIN RESULTS OF THE CHAIN
+###
+sigmacb=sigmacb[,3]
+xicb=xicb[,3]
+threshold=thresholds[3]
+
+estim = cbind( sigmacb, xicb)
+ests <- c(mean(estim[, 1]), mean(estim[, 2]))
+ests1 <- c(median(estim[, 1]), median(estim[, 2]))
+ests2 <- array(0, c(2, 2))
+ests2[1, ] <- c(quantile(estim[, 1], 0.025), quantile(estim[, 2], 0.025))
+ests2[2, ] <- c(quantile(estim[, 1], 0.975), quantile(estim[, 2], 0.975))
+results <- list(posterior = estim, data = y, postmean = ests, 
+                postmedian = ests1, postCI = ests2)
+names(results$postmean) <- c("sigma", "xi")
+names(results$postmedian) <- c( "sigma", "xi")
+dimnames(results$postCI) <- list(c("lower bound", "upper bound"), 
+                                 c( "sigma", "xi"))
+class(results) <- "gpdp"
+
+###
+#RETURN LEVEL COMPUTATION AND PLOT
+###
+
+
+t <- 2  #start ret level
+k <- 100   #end ret level
+x <- results
+
+sampl <- qgpd(1 - 1/t, x$posterior[, 2], threshold, x$posterior[, 1])
+res <- quantile(sampl, 0.5)
+ta <- seq(1, k, 1)
+n <- length(ta)
+li <- array(0, c(n))
+ls <- array(0, c(n))
+pred <- array(0, c(n))
+for (s in 1:n) {
+  sampl <- qgpd(1 - 1/s, x$posterior[, 2], threshold, x$posterior[, 1])
+  li[s] <- quantile(sampl, 0.025)
+  ls[s] <- quantile(sampl, 0.975)
+  pred[s] <- quantile(sampl, 0.5)
+}
+x11()
+ggplot(data = data.frame( level= ta, intensity=pred), mapping = aes(x=level,y=intensity)) +
+  geom_line( color = "red") +
+  xlab("level") + 
+  ylab("wind speed (kmh)") + 
+  ggtitle("Return level") +
+  xlim(0,100)+
+  ylim(80,195) +
+  geom_line(aes(y = li), color = "black", linetype = "dotted") +
+  geom_line(aes(y = ls), color = "black", linetype = "dotted")
+
+
+##
+#log predictive density
+##
+
+data = x$data[x$data > threshold]
+linf = min(data)
+lsup = max(data)
+dat1 = seq(linf, lsup, (lsup - linf)/300)
+n = length(dat1)
+int = length(x$posterior[, 1])
+res = array(0, c(n))
+for (i in 1:n) {
+  for (j in 1:int) {
+    res[i] = res[i] + (1/int) * dgpd(dat1[i], x$posterior[j, 
+                                                          2], thresholds[1], x$posterior[j, 1])
+  }
+}
+dataf <- data.frame(log_pos = dat)
+dataf2 <- data.frame(data = dat1, res = res)
+
+ggplot(dataf, aes(x=log_pos)) + 
+  geom_histogram(aes(y = ..density.. ), bins = 20, color = "black", fill = "lightblue") +
+  xlab("data") + 
+  ylab("density") +
+  ggtitle("density") +
+  geom_line(aes(x=data,y = res), dataf2,color = "red") 
+
+
+
+
+
+
+
